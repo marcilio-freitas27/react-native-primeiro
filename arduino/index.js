6bd12bc41e6f0ca4605cbc62c545cd440f7820e8
Marcilio Freitas <marcilio@beltsistemas.com.br>
2022-09-21 18:22:22 -0300

    Remoção de código c++, alterações no arquivo index do arduino, conexão com arduino e node está funcional.

--- arduino/index.js
+++ arduino/index.js
@@ -1,33 +1,23 @@
-// var five = context.global.jfive;    // create a shorter alias
-// var led = new five.Led(13);         // instatiate the led
-// led.blink(500);                     // blink it every 500 ms
-// var five = require("johnny-five"),
-//     board = new five.Board();
+const { Board, Led } = require("johnny-five");
+const board = new Board();
 
-// board.on("ready", function() {
-//   // Create an Led on pin 13
-//   var led = new five.Led(13);
-
-//   // Strobe the pin on/off, defaults to 100ms phases
-//   led.strobe();
-// });
-
-const { Board } = require("johnny-five");
-
-// Johnny-Five will try its hardest to detect the port for you,
-// however you may also explicitly specify the port by passing
-// it as an optional property to the Board constructor:
-const board = new Board({
-  port: "/dev/cu.usbmodem1411"
-});
-
-// The board's pins will not be accessible until
-// the board has reported that it is ready
 board.on("ready", () => {
-  board.pinMode(13, board.MODES.OUTPUT);
-
-  board.loop(500, () => {
-    // Whatever the last value was, write the opposite
-    board.digitalWrite(13, board.pins[13].value ? 0 : 1);
+  const led = new Led(13);
+  board.repl.inject({
+    on: function(){
+      led.on();
+    },
+    off: function(){
+      led.off();
+    },
+    strobe: function(){
+      led.strobe();
+    },
+    blink: function(){
+      led.blink();
+    },
+    stop: function(){
+      led.stop();
+    }
   });
 });
